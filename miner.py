a148faa7f4fc80fe3d882f2d60114f7cb673076b
sophiane15 <153296722+sophiane15@users.noreply.github.com>
2025-07-08 15:02:01 +0200

    Update miner.py

--- miner.py
+++ miner.py
@@ -1,1 +1,14 @@
+from blockchain import Blockchain
 
+def main():
+    blockchain = Blockchain()
+    
+    # Miner les transactions en attente
+    if blockchain.pending_transactions:
+        blockchain.mine_pending_transactions()
+        print("Dernier bloc miné :", blockchain.chain[-1].__dict__)
+    else:
+        print("Aucune transaction à miner !")
+
+if __name__ == "__main__":
+    main()
